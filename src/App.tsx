fe3cfbcb851c5c178330c9a821b024d1ebb52a2f
John Chen <john.data.chen@gmail.com>
2024-11-28 15:15:34 +0800

    Add fake data if local storage doesn't have value

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,10 @@ const Title = styled.h1`
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>(
-    JSON.parse(localStorage.getItem("todos") || "[]")
+    JSON.parse(
+      localStorage.getItem("todos") ||
+        `[{"id":"1732777773493yllerj6b","text":"1: delete fake data before using","dueDate":"2024/11/28","completed":false},{"id":"1732777775878vnba3lt5","text":"2","dueDate":"","completed":true},{"id":"1732777800954jpxnw26l","text":"3","dueDate":"2024/11/30","completed":false}]`
+    )
   );
 
   const addTodo = function (text: string, dueDate: Date | string | null) {
