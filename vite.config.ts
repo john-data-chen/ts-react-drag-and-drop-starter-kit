22e6c94dcc39364bf1a6f4f76379516090e77c79
John Chen <john.data.chen@gmail.com>
2024-12-15 14:09:43 +0800

    Remove vite-remove-attribute which stops deployment action

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import viteCompression from "vite-plugin-compression";
-import removeAttribute from "@castlenine/vite-remove-attribute";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,11 +10,6 @@ export default defineConfig({
     viteCompression({
       threshold: 50000,
       algorithm: "gzip",
-    }),
-    removeAttribute({
-      extensions: ["ts", "tsx"],
-      attributes: ["data-testid"],
-      ignoreFolders: ["./src/__tests__"],
     }),
   ],
 });
