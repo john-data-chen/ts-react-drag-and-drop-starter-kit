edd542a4c6a9302bb15bdfc5c31c7385f3abbeba
John Chen <john.data.chen@gmail.com>
2024-12-16 15:34:42 +0800

    Add testid and aria label

--- src/component/AppTitle.tsx
+++ src/component/AppTitle.tsx
@@ -2,5 +2,9 @@ import { useTranslation } from "react-i18next";
 
 export default function AppTitle() {
   const { t } = useTranslation();
-  return <h1 className="appTitle">{t("app-title")}</h1>;
+  return (
+    <h1 className="appTitle" data-testid="appTitle" aria-label={t("app-title")}>
+      {t("app-title")}
+    </h1>
+  );
 }
