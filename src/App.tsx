934c63eaac7d173259c0b555fbc71c0c24b3d099
John Chen <john.data.chen@gmail.com>
2024-12-16 13:36:06 +0800

    Refactor darg and drop from APP

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,27 @@
 import { ThemeProvider } from "styled-components";
 import TodoForm from "./component/TodoForm";
-import TodoList from "./component/TodoList";
 import { lightTheme, darkTheme, GlobalStyles } from "./theme/ThemeSets";
-import {
-  DragDropContext,
-  Draggable,
-  Droppable,
-  DropResult,
-} from "@hello-pangea/dnd";
+import { DragDropContext } from "@hello-pangea/dnd";
 import { useTranslation } from "react-i18next";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  addTodo,
-  editTodo,
-  deleteTodo,
-  handleDragEnd,
-  toggleComplete,
-} from "./redux/todoSlice";
-import { RootState } from "./redux/store";
 import LanguageSelector from "./component/LanguageSelector";
 import { useLanguage } from "./hooks/useLanguage";
 import ThemeToggle from "./component/ThemeToggle";
 import { useTheme } from "./hooks/useTheme";
+import { useTodos } from "./hooks/useTodos";
+import TodoDragDropList from "./component/TodoDragDropList";
 
 function App() {
-  const todosSelector = useSelector((state: RootState) => state.todos.todos);
+  const {
+    todos,
+    handleAddTodo,
+    handleDragEndTodos,
+    handleToggleComplete,
+    handleDeleteTodo,
+    handleEditTodo,
+  } = useTodos();
   const { isDarkMode, handleSwitchTheme } = useTheme();
   const { languageCode, handleLanguageChange } = useLanguage();
   const { t } = useTranslation();
-  const dispatch = useDispatch();
-
-  const handleAddTodo = (text: string, dueDate: Date | null) => {
-    const dueDateStr = dueDate ? dueDate.toISOString() : null;
-    dispatch(addTodo({ text, dueDate: dueDateStr }));
-  };
-
-  const onDragEnd = (event: DropResult) => {
-    const { source, destination } = event;
-    if (!destination) return;
-    const newTodos = [...todosSelector];
-    const [removed] = newTodos.splice(source.index, 1);
-    newTodos.splice(destination.index, 0, removed);
-    dispatch(handleDragEnd(newTodos));
-  };
-
-  const handleToggleComplete = (id: string) => {
-    dispatch(toggleComplete(id));
-  };
-
-  const handleDeleteTodo = (id: string) => {
-    dispatch(deleteTodo(id));
-  };
-
-  const handleEditTodo = (id: string, text: string, dueDate: Date | null) => {
-    const dueDateStr = dueDate ? dueDate.toISOString() : "";
-    dispatch(editTodo({ id, text, dueDate: dueDateStr }));
-  };
 
   return (
     <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
@@ -73,37 +39,13 @@ function App() {
         </div>
         <h1 className="appTitle">{t("app-title")}</h1>
         <TodoForm addTodo={handleAddTodo} />
-        <DragDropContext onDragEnd={onDragEnd}>
-          <Droppable droppableId="drop-id">
-            {(provided) => (
-              <div ref={provided.innerRef} {...provided.droppableProps}>
-                {todosSelector.map((todo, i) => (
-                  <div key={todo.id}>
-                    <Draggable draggableId={todo.id} index={i} key={todo.id}>
-                      {(provided) => (
-                        <div
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                          ref={provided.innerRef}
-                        >
-                          {
-                            <TodoList
-                              todos={[todo]}
-                              key={todo.id}
-                              toggleComplete={handleToggleComplete}
-                              deleteTodo={handleDeleteTodo}
-                              handleEditTodo={handleEditTodo}
-                            />
-                          }
-                        </div>
-                      )}
-                    </Draggable>
-                  </div>
-                ))}
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
+        <DragDropContext onDragEnd={handleDragEndTodos}>
+          <TodoDragDropList
+            todos={todos}
+            handleToggleComplete={handleToggleComplete}
+            handleDeleteTodo={handleDeleteTodo}
+            handleEditTodo={handleEditTodo}
+          />
         </DragDropContext>
       </div>
     </ThemeProvider>
---
+++ src/hooks/useTodos.ts
@@ -0,0 +1,47 @@
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "../redux/store";
+import {
+  addTodo,
+  handleDragEnd,
+  toggleComplete,
+  deleteTodo,
+  editTodo,
+} from "../redux/todoSlice";
+import { DropResult } from "@hello-pangea/dnd";
+
+export const useTodos = () => {
+  const todos = useSelector((state: RootState) => state.todos.todos);
+  const dispatch = useDispatch();
+
+  const handleAddTodo = (text: string, dueDate: Date | null) => {
+    const dueDateStr = dueDate ? dueDate.toISOString() : null;
+    dispatch(addTodo({ text, dueDate: dueDateStr }));
+  };
+
+  const handleDragEndTodos = (event: DropResult) => {
+    const { source, destination } = event;
+    if (!destination) return;
+
+    const newTodos = [...todos];
+    const [removed] = newTodos.splice(source.index, 1);
+    newTodos.splice(destination.index, 0, removed);
+    dispatch(handleDragEnd(newTodos));
+  };
+
+  const handleToggleComplete = (id: string) => dispatch(toggleComplete(id));
+  const handleDeleteTodo = (id: string) => dispatch(deleteTodo(id));
+
+  const handleEditTodo = (id: string, text: string, dueDate: Date | null) => {
+    const dueDateStr = dueDate ? dueDate.toISOString() : "";
+    dispatch(editTodo({ id, text, dueDate: dueDateStr }));
+  };
+
+  return {
+    todos,
+    handleAddTodo,
+    handleDragEndTodos,
+    handleToggleComplete,
+    handleDeleteTodo,
+    handleEditTodo,
+  };
+};
