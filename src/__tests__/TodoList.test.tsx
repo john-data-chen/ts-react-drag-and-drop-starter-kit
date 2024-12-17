2ce818f5683bd684afd58ad2d18f085810ea8e89
John Chen <john.data.chen@gmail.com>
2024-12-17 15:13:03 +0800

    Add unit test of TodoList

---
+++ src/__tests__/TodoList.test.tsx
@@ -0,0 +1,48 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import TodoList from "../component/TodoList";
+import { DEMO_TASKS } from "../constants/constants";
+
+jest.mock("react-i18next", () => ({
+  useTranslation: () => ({
+    t: (key: string) => key,
+  }),
+}));
+
+describe("TodoList", () => {
+  const mockTodos = DEMO_TASKS;
+  const mockToggleComplete = jest.fn();
+  const mockDeleteTodo = jest.fn();
+  const mockHandleEditTodo = jest.fn();
+
+  test("test_todo_list_renders_all_items", () => {
+    render(
+      <TodoList
+        todos={mockTodos}
+        toggleComplete={mockToggleComplete}
+        deleteTodo={mockDeleteTodo}
+        handleEditTodo={mockHandleEditTodo}
+      />
+    );
+
+    const todoList = screen.getByTestId("todoList");
+    expect(todoList).toBeInTheDocument();
+    expect(todoList.children).toHaveLength(3);
+  });
+
+  test("test_todo_list_passes_correct_props", () => {
+    const { container } = render(
+      <TodoList
+        todos={mockTodos}
+        toggleComplete={mockToggleComplete}
+        deleteTodo={mockDeleteTodo}
+        handleEditTodo={mockHandleEditTodo}
+      />
+    );
+
+    const todoCards = container.getElementsByClassName("todoList")[0].children;
+    expect(todoCards).toHaveLength(mockTodos.length);
+
+    expect(todoCards[0]).toHaveTextContent(mockTodos[0].text);
+  });
+});
