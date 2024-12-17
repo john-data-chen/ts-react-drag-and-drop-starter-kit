a7fb6fc0418ab6c652cd1e37219d6e409c9f23d2
John Chen <john.data.chen@gmail.com>
2024-12-17 11:24:58 +0800

    Add memo to improve performance

--- src/component/LanguageSelector.tsx
+++ src/component/LanguageSelector.tsx
@@ -7,31 +7,30 @@ interface LanguageSelectorProps {
   onChangeLang: (languageCode: string) => void;
 }
 
-const LanguageSelector: React.FC<LanguageSelectorProps> = ({
-  selectedLanguage,
-  onChangeLang,
-}) => {
-  const { t } = useTranslation();
-  return (
-    <select
-      className="languageSelector"
-      data-testid="languageSelector"
-      value={selectedLanguage}
-      onChange={(e) => onChangeLang(e.target.value)}
-      aria-label={t("language-selector.aria-label")}
-    >
-      {LANGUAGES.map((lang) => (
-        <option
-          key={lang.code}
-          value={lang.code}
-          data-testid={`LanguageOption-${lang.code}`}
-          aria-label={t("language-selector.aria-label") + ": " + lang.label}
-        >
-          {lang.label}
-        </option>
-      ))}
-    </select>
-  );
-};
+const LanguageSelector: React.FC<LanguageSelectorProps> = React.memo(
+  ({ selectedLanguage, onChangeLang }) => {
+    const { t } = useTranslation();
+    return (
+      <select
+        className="languageSelector"
+        data-testid="languageSelector"
+        value={selectedLanguage}
+        onChange={(e) => onChangeLang(e.target.value)}
+        aria-label={t("language-selector.aria-label")}
+      >
+        {LANGUAGES.map((lang) => (
+          <option
+            key={lang.code}
+            value={lang.code}
+            data-testid={`LanguageOption-${lang.code}`}
+            aria-label={`${t("language-selector.aria-label")}: ${lang.label}`}
+          >
+            {lang.label}
+          </option>
+        ))}
+      </select>
+    );
+  }
+);
 
 export default LanguageSelector;
