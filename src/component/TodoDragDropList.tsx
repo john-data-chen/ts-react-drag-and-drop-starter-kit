b5ec880671b682588fc5cf8b2905146e33bdd976
John Chen <john.data.chen@gmail.com>
2024-12-17 23:50:13 +0800

    Refactor for unit test

--- src/component/TodoDragDropList.tsx
+++ src/component/TodoDragDropList.tsx
@@ -1,3 +1,4 @@
+import { memo } from "react";
 import Todo from "../type/Todo";
 import TodoList from "./TodoList";
 import { Draggable, Droppable } from "@hello-pangea/dnd";
@@ -9,44 +10,46 @@ interface TodoDragDropListProps {
   handleEditTodo: (id: string, text: string, dueDate: Date | null) => void;
 }
 
-const TodoDragDropList = ({
-  todos,
-  handleToggleComplete,
-  handleDeleteTodo,
-  handleEditTodo,
-}: TodoDragDropListProps) => {
-  return (
-    <Droppable droppableId="drop-id">
-      {(provided) => (
-        <div ref={provided.innerRef} {...provided.droppableProps}>
-          {todos.map((todo, i) => (
-            <div key={todo.id}>
-              <Draggable draggableId={todo.id} index={i} key={todo.id}>
+const TodoDragDropList = memo(
+  ({
+    todos,
+    handleToggleComplete,
+    handleDeleteTodo,
+    handleEditTodo,
+  }: TodoDragDropListProps) => {
+    return (
+      <Droppable droppableId="drop-id" data-testid="dragDropList">
+        {(provided) => (
+          <div ref={provided.innerRef} {...provided.droppableProps}>
+            {todos.map((todo, i) => (
+              <Draggable
+                draggableId={todo.id}
+                index={i}
+                key={todo.id}
+                data-testid={`draggable-${todo.id}`}
+              >
                 {(provided) => (
                   <div
                     {...provided.draggableProps}
                     {...provided.dragHandleProps}
                     ref={provided.innerRef}
                   >
-                    {
-                      <TodoList
-                        todos={[todo]}
-                        key={todo.id}
-                        toggleComplete={handleToggleComplete}
-                        deleteTodo={handleDeleteTodo}
-                        handleEditTodo={handleEditTodo}
-                      />
-                    }
+                    <TodoList
+                      todos={[todo]}
+                      toggleComplete={handleToggleComplete}
+                      deleteTodo={handleDeleteTodo}
+                      handleEditTodo={handleEditTodo}
+                    />
                   </div>
                 )}
               </Draggable>
-            </div>
-          ))}
-          {provided.placeholder}
-        </div>
-      )}
-    </Droppable>
-  );
-};
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    );
+  }
+);
 
 export default TodoDragDropList;
