0a5faea9592d5fe8ec2894cdbfe657a5e5fd9d48
John Chen <john.data.chen@gmail.com>
2024-12-17 15:12:47 +0800

    Refactor and add aria label for unit test

--- src/component/TodoList.tsx
+++ src/component/TodoList.tsx
@@ -1,3 +1,4 @@
+import { memo } from "react";
 import TodoCard from "./TodoCard";
 import Todo from "../type/Todo";
 
@@ -8,25 +9,33 @@ interface TodoListProps {
   handleEditTodo: (id: string, text: string, dueDate: Date | null) => void;
 }
 
-const TodoList = ({
-  todos,
-  toggleComplete,
-  deleteTodo,
-  handleEditTodo,
-}: TodoListProps) => {
-  return (
-    <ul className="todoList">
-      {todos.map((todo) => (
-        <TodoCard
-          key={todo.id}
-          todo={todo}
-          toggleComplete={toggleComplete}
-          deleteTodo={deleteTodo}
-          handleEditTodo={handleEditTodo}
-        />
-      ))}
-    </ul>
-  );
-};
+interface TodoListProps {
+  todos: Todo[];
+  toggleComplete: (id: string) => void;
+  deleteTodo: (id: string) => void;
+  handleEditTodo: (id: string, text: string, dueDate: Date | null) => void;
+}
+
+const TodoList = memo(
+  ({ todos, toggleComplete, deleteTodo, handleEditTodo }: TodoListProps) => {
+    return (
+      <ul
+        className="todoList"
+        data-testid="todoList"
+        aria-label="Todo items list"
+      >
+        {todos.map((todo) => (
+          <TodoCard
+            key={todo.id}
+            todo={todo}
+            toggleComplete={toggleComplete}
+            deleteTodo={deleteTodo}
+            handleEditTodo={handleEditTodo}
+          />
+        ))}
+      </ul>
+    );
+  }
+);
 
 export default TodoList;
