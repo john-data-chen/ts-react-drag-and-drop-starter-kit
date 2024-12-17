345556ab80f0f412ca209c23d8d728b3f7f8bcc3
John Chen <john.data.chen@gmail.com>
2024-12-17 14:31:32 +0800

    Add test of TodoForm

---
+++ src/__tests__/TodoForm.test.tsx
@@ -0,0 +1,40 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import TodoForm from "./../component/TodoForm";
+
+jest.mock("react-i18next", () => ({
+  useTranslation: () => ({
+    t: (key: string) => key,
+  }),
+}));
+
+describe("TodoForm", () => {
+  const mockAddTodo = jest.fn();
+
+  beforeEach(() => {
+    mockAddTodo.mockClear();
+  });
+
+  it("test_submit_valid_todo_with_date", async () => {
+    render(<TodoForm addTodo={mockAddTodo} />);
+
+    const input = screen.getByTestId("addTodoInput");
+    const submitButton = screen.getByTestId("addTaskButton");
+
+    fireEvent.change(input, { target: { value: "Test todo" } });
+    fireEvent.click(submitButton);
+
+    expect(mockAddTodo).toHaveBeenCalledWith("Test todo", null);
+  });
+
+  it("test_submit_valid_todo_without_date", async () => {
+    render(<TodoForm addTodo={mockAddTodo} />);
+
+    const input = screen.getByTestId("addTodoInput");
+    const submitButton = screen.getByTestId("addTaskButton");
+
+    fireEvent.change(input, { target: { value: "" } });
+    fireEvent.click(submitButton);
+
+    expect(mockAddTodo).not.toHaveBeenCalled();
+  });
+});
