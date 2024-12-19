5c962e65e357e74f3712e4a44a022fedea2d2a6e
JianAn Chen <john-data-chen@users.noreply.github.com>
2024-12-19 16:22:12 +0800

    Reduce data of demo tasks

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,17 +1,16 @@
 const DEMO_TASKS = [
   {
     id: '1733313427660',
-    text: '1: Delete the Demo Tasks before you use',
+    text: 'Delete the Demo Tasks before you use',
     dueDate: null,
     completed: false
   },
   {
     id: '1733313461490',
-    text: '2: You can drag to re-order tasks',
+    text: 'You can drag to re-order tasks',
     dueDate: '2024-12-03T16:00:00.000Z',
-    completed: false
-  },
-  { id: '1733313502106', text: '3: Try it!', dueDate: null, completed: true }
+    completed: true
+  }
 ]
 
 const LANGUAGES = [
